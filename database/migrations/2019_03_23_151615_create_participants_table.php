b75583ebfe8eed475c06bbac14a5491c0887add5
mtsdalmolin <mtsdalmolin@gmail.com>
2019-03-27 11:44:53 -0300

    tables participants + tables presence

---
+++ database/migrations/2019_03_23_151615_create_participants_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateParticipantsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('participants', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name', 50);
+            $table->string('type', 20);
+            $table->string('email', 45);
+            $table->string('matricula', 12);
+            $table->integer('activity_id')->unsigned();
+            $table->foreign('activity_id')->references('id')->on('activities');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('participants');
+    }
+}
---
+++ database/migrations/2019_03_23_152123_create_presence_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePresenceTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('presence', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('activity_day_id')->unsigned();
+            $table->foreign('activity_day_id')->references('id')->on('activity_days');
+            $table->integer('participant_id')->unsigned();
+            $table->foreign('participant_id')->references('id')->on('participants');
+	    $table->boolean('presence_mark');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('presence');
+    }
+}
