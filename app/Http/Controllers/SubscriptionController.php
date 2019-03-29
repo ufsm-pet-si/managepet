4c07398b0f612e4b7e96c55338098d8b553bc2a6
mtsdalmolin <mtsdalmolin@gmail.com>
2019-03-29 17:19:00 -0300

    subscription controller updated

--- app/Http/Controllers/SubscriptionController.php
+++ app/Http/Controllers/SubscriptionController.php
@@ -24,4 +24,102 @@ class SubscriptionController extends Controller
         // load the views and pass the participants
         return View::make('subscription.list')->with('participants', $participant);
     }
+    
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        // load the create form (views/categories/form.blade.php)
+        return View::make('categories.form');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request, Subscription $subscription)
+    {
+        // store
+        if (
+        Subscription::create($request->input('cadastro'))) {
+            Session::flash('message', ['text'=>"EITA", 'type'=>"success"]);
+        }
+        // redirect
+        Session::flash('message', ['text'=>"Usuário cadastrado com sucesso!", 'type'=>"success"]);
+        return redirect()->route('subscription.index');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Category  $category
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Category $category)
+    {
+        // get the category
+        $searchedCategory = Category::find($category)->first();
+
+        // show the view and pass the category to it
+        return View::make('categories.form')->with('category', $searchedCategory);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Category  $category
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        // get the category
+        $searchedCategory = Category::find($id);
+
+        // show the edit form and pass the category
+        return View::make('categories.form')->with('category', $searchedCategory);     
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Category  $category
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Category $category)
+    {
+        $request->validate([
+            'name'          => 'required',
+            'search_center' => 'required',
+            'type'          => 'required'
+        ]);
+
+        // store
+        $category->update($request->all());
+
+        // redirect
+        Session::flash('message', ['text'=>"Categoria atualizada com sucesso!", 'type'=>"success"]);
+        return redirect()->route('categories.index');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Category  $category
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Category $category)
+    {
+        // delete
+        $category->delete();
+
+        // redirect
+        Session::flash('message', ['text'=>"Categoria removida com sucesso!", 'type'=>"success"]);
+        return redirect()->route('categories.index');
+    }
 }
